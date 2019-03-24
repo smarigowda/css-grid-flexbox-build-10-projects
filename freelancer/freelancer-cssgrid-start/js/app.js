e8dd2a3c16c9711aea102c4af4a6dea4c4c9dbd2
Santosh Marigowda <santosharakere@gmail.com>
2019-03-24 21:03:56 +0000

    BUILD10-16 constructing html using data

--- freelancer/freelancer-cssgrid-start/js/app.js
+++ freelancer/freelancer-cssgrid-start/js/app.js
@@ -7,5 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             console.log(data.portfolio);
+            // build a template
+            let html = '';
+            data.portfolio.forEach(item => {
+                html += `
+                    <div class="item">
+                        <img src="img/${item.id}.jpg"/>
+                        <div class="content">
+                            <h3>${item.projectname}</h3>
+                            <p>${item.desc}</p>
+                        </div>
+                    </div>
+                `;
+            });
+            console.log(html);
+            document.querySelector('#portfolio').innerHTML = html;
         })
 });
