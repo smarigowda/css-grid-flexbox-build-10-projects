e88478f44c29dd98fe606d077812207cfff92277
Santosh Marigowda <santosharakere@gmail.com>
2019-03-24 20:43:02 +0000

    BUILD10-16 reading data from data.json

--- freelancer/freelancer-cssgrid-start/js/app.js
+++ freelancer/freelancer-cssgrid-start/js/app.js
@@ -1,1 +1,11 @@
-document;
+console.log('reading data from data.json');
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOMContentLoaded fired');
+    fetch('data.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.portfolio);
+        })
+});
